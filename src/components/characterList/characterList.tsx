46376e86b7f2ec437e2ad22ddfdde175bccde59c
Lucas <112008978+LucasMaurojorgee@users.noreply.github.com>
2023-05-09 16:06:58 -0300

    character list (#2)

---
+++ src/components/characterList/characterList.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { CharacterCard } from "./CharacterCard";
+import { Character } from "./iCharacter";
+import axios from "axios";
+
+type GetCharacterResponse = {
+  info: { count: number; pages: number; next?: string; prev?: string };
+  results: Array<Character>;
+};
+
+async function getCharacters() {
+  const { data } = await axios.get<GetCharacterResponse>(
+    "https://rickandmortyapi.com/api/character"
+  );
+  return data;
+}
+
+export const CharacterList = () => {
+  const [characters, setCharacters] = useState<Array<Character>>([]);
+
+  useEffect(() => {
+    let mounted = true;
+
+    getCharacters()
+      .then((items) => {
+        if (mounted) {
+          setCharacters(items.results);
+        }
+      })
+      .catch((error) => console.log(error));
+
+    return (): void => {
+      mounted = false;
+    };
+  }, []);
+
+  return (
+    <div className="bg-gray-800 flex flex-wrap justify-center">
+      {characters?.map((character) => (
+        <CharacterCard character={character} />
+      ))}
+    </div>
+  );
+};
---
+++ src/components/characterList/iCharacter.tsx
@@ -0,0 +1,9 @@
+export type Character = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  origin: { name: string };
+  location: { name: string };
+  image: string;
+};
